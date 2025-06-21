a51aa038fdafad493047de1a8520060be923fafe
David Mason <emamasonn@gmail.com>
2025-06-20 21:00:38 -0300

    test

--- src/app/(lab)/scroll/utils.tsx
+++ src/app/(lab)/scroll/utils.tsx
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 
-export const GenerateInitMaterials = (colorsMaterial) => {
+export const GenerateInitMaterials = () => {
   const falloutBoyTexture = new THREE.TextureLoader().load(
     "/assets/scroll/FalloutBoy.png"
   );
